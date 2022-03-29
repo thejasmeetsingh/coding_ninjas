a2a60dd153e00cd78089b88d530c5180c3e5976b
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2022-03-29 22:13:17 +0530

    Solved Count Nodes Problem

---
+++ trees/count_nodes.py
@@ -0,0 +1,13 @@
+int getLargeNodeCount(TreeNode<int>* root, int x) {
+    int count = 0;
+    
+    if (root != NULL) {
+        if (root -> data > x) {
+            count++;
+        }
+        for (int i=0; i < root -> children.size(); i++) {
+            count += getLargeNodeCount(root -> children[i], x);
+        }
+    }
+    return count;
+}
