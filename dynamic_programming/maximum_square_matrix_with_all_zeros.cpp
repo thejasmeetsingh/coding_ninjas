b5830dd21c5ec93b00a5c91a5ad994604f5001a8
Jasmeet Singh <51269010+thejasmeetsingh@users.noreply.github.com>
2022-06-01 22:24:03 +0530

    Solved Maximum Square Matrix With All Zeros Problem

---
+++ dynamic_programming/maximum_square_matrix_with_all_zeros.cpp
@@ -0,0 +1,25 @@
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int findMaxSquareWithAllZeros(int** arr, int row, int col){
+    for (int i=0;i<row;i++)
+        
+    { 
+        for (int j=0;j<col;j++)
+        
+       if (arr[i][j]==1)
+           arr[i][j]=0; 
+          else arr[i][j]=1;
+       }
+    int max=0;
+    for (int i=1;i<row;i++)
+    {  for (int j=1;j<col;j++)
+         {    if (arr[i][j]==0) continue;
+             arr[i][j]=min(arr[i-1][j],min(arr[i-1][j-1],arr[i][j-1]))+1;
+              if (max<arr[i][j]) max=arr[i][j];
+         }
+      
+    }
+    
+   return max; 
+}
